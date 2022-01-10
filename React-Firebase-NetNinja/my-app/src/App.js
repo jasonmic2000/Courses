35f6bd74861ad8cb2d76c385bb28c958943f7f28
Jason Michael <jason_mic2000@yahoo.com>
2022-01-10 14:23:09 +0530

    useState React app

--- React-Firebase-NetNinja/my-app/src/App.js
+++ React-Firebase-NetNinja/my-app/src/App.js
@@ -2,17 +2,42 @@ import './App.css';
 import { useState } from 'react'
 
 function App() {
-  const [name, setName] = useState('Mario')
+  const [showEvents, setShowEvents] = useState(true)
+  const [events, setEvents] = useState([
+    {title: "mario's birthday bash", id: 1},
+    {title: "bowser's live stream", id: 2},
+    {title: "race on moo moo farm", id: 3}
+  ])
 
-  const handleClick = () => {
-    setName('Luigi')
-    console.log(name)
+  console.log(showEvents)
+
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id
+      })
+    })
+    console.log(id)
   }
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>hide events</button>
+        </div>
+      )}
+      {!showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(true)}>show events</button>
+        </div>
+      )}
+      {showEvents && events.map((event, index) => (
+        <div key={event.id}>
+          <h2>{index} - {event.title}</h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
+        </div>
+      ))}
     </div>
   );
 }
