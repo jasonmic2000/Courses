1f6ad8a88b46e7e323a6fd115e4121ca4b5f40b6
Jason Michael <jason_mic2000@yahoo.com>
2022-01-17 17:44:17 +0530

    Aborting fetch request using cleanup function

--- React-Firebase-NetNinja/tripago/src/hooks/useFetch.js
+++ React-Firebase-NetNinja/tripago/src/hooks/useFetch.js
@@ -6,11 +6,13 @@ export const useFetch = (url) => {
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        const controller = new AbortController()
+
         const fetchData = async () => {
             setIsPending(true)
 
             try {
-                const res = await fetch(url)
+                const res = await fetch(url, { signal: controller.signal })
                 if (!res.ok) {
                     throw new Error(res.statusText)
                 }
@@ -21,14 +23,21 @@ export const useFetch = (url) => {
                 setError(null)
 
             } catch (err) {
-                setIsPending(false)
-                setError('Could not fetch data.')
-                console.log(err.message)
+                if (err.name === "AbortError") {
+                    console.log('the fetch was aborted')
+                } else {
+                    setIsPending(false)
+                    setError('Could not fetch data.')
+                }
             }
             
         }
 
         fetchData()
+
+        return () => {
+            controller.abort()
+        }
 
     }, [url])
 
